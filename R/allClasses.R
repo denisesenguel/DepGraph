b141a02d008da4d5a097d454d421f0b5f9fcc7af
Denise <denise@omni.tt>
2016-04-26 14:11:09 +0200

    added some notes for the procedure

--- R/allClasses.R
+++ R/allClasses.R
@@ -4,3 +4,17 @@
 # in a package namespace. note: also hidden functions!
 # 2) determine what aspects can be set by the user: color-coding, subset of objects or all, central
 # element(s), .. shape of boxes, whether to use boxes at all..
+# GUI wo man sachen verschieben kann wäre nice
+
+# INPUT ARGUMENTS:
+#   - 'package': need to have a fcn that: checks if package is loaded/installed. if not:load/inst
+
+
+# method(basic):
+#   - ls(envir = as.environment("package:packagename"))
+#   - then sort by class! difficult with hidden functions, since get() doesn't work. maybe need
+#     to append ":::"
+# for function: find out if function calls another function by printing and searching its text
+
+# how are mindmaps/flowcharts created using R? maybe other nice visualizations?
+# package 'diagram' might be worth looking into
